6f6ed5d264fb219de3957a8b177069eec512af8a
CWJ <cozycoxy@naver.com>
2021-02-21 14:34:33 +0900

    닉네임 변경 요청 화면 호출     -startActivityForResult

---
+++ app/src/main/java/com/tjoeun/android/intent_20210221/ChangeNickName.kt
@@ -0,0 +1,11 @@
+package com.tjoeun.android.intent_20210221
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ChangeNickName : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_change_nick_name)
+    }
+}
--- app/src/main/java/com/tjoeun/android/intent_20210221/MainActivity.kt
+++ app/src/main/java/com/tjoeun/android/intent_20210221/MainActivity.kt
@@ -6,6 +6,9 @@ import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    // 닉네임변경 - 1001 이라는것을 아에 변수로넣어두고 ㅏ상요
+    val REQ_FOR_NICKNAME = 1001
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -16,16 +19,26 @@ class MainActivity : AppCompatActivity() {
         }
 
         sendContentBtn.setOnClickListener {
+
             //입력한문구 String으로 추출
             val inputContent = contentEdit.text.toString()
 
-            // 두번째 화면
+            // 두번째 화면으로 전달
             val myIntent = Intent(this, SecondActivity::class.java)
 
             myIntent.putExtra("message", inputContent)
-            myIntent.putExtra("출생년도", "1988")
+            myIntent.putExtra("출생년도", 1988)
 
             startActivity(myIntent)
+        }
+
+        changeNickName.setOnClickListener {
+
+            // 새 닉네임을 얻기 위해 => 액티비티 이동
+            val myIntent = Intent(this, ChangeNickName::class.java)
+
+            // 어떤 데이터를 받으로 가는건지 숫자로 구별 ㄷㅌ
+            startActivityForResult(myIntent, REQ_FOR_NICKNAME)
         }
     }
 }
