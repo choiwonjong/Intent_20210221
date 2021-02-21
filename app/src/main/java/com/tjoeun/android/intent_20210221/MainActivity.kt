b5df01c89d15f14ff03c9e05c327099e5df0c564
CWJ <cozycoxy@naver.com>
2021-02-21 13:51:27 +0900

    두번째버튼추가 - 화면이동시 데이터 첨부

--- app/src/main/java/com/tjoeun/android/intent_20210221/MainActivity.kt
+++ app/src/main/java/com/tjoeun/android/intent_20210221/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.tjoeun.android.intent_20210221
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
@@ -14,5 +15,16 @@ class MainActivity : AppCompatActivity() {
             startActivity(myIntent)
         }
 
+        sendContentBtn.setOnClickListener {
+            //입력한문구 String으로 추출
+            val inputContent = contentEdit.text.toString()
+
+            // 두번째 화면
+            val myIntent = Intent(this, SecondActivity::class.java)
+            myIntent.putExtra("message", inputContent)
+            myIntent.putExtra("출생년도", "1988")
+
+            startActivity(myIntent)
+        }
     }
 }
