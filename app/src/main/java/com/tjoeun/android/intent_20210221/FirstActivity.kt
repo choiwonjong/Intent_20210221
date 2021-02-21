7e90708e468c2b5d5d50b3ed92728ced7096dcf8
CWJ <cozycoxy@naver.com>
2021-02-21 13:35:32 +0900

    메인화면 복귀 버튼

--- app/src/main/java/com/tjoeun/android/intent_20210221/FirstActivity.kt
+++ app/src/main/java/com/tjoeun/android/intent_20210221/FirstActivity.kt
@@ -2,10 +2,16 @@ package com.tjoeun.android.intent_20210221
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_first.*
 
 class FirstActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_first)
+
+        backToMainBtn.setOnClickListener {
+            //이전화면으로 돌아가기
+            finish();
+        }
     }
 }
--- app/src/main/java/com/tjoeun/android/intent_20210221/MainActivity.kt
+++ app/src/main/java/com/tjoeun/android/intent_20210221/MainActivity.kt
@@ -2,10 +2,17 @@ package com.tjoeun.android.intent_20210221
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        moveToFirstActivity.setOnClickListener {
+            var myIntent = Intent(this, FirstActivity::class.java)
+            startActivity(myIntent)
+        }
+
     }
 }
